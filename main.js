e529c1d0235978e7191ddccfefd1d78352d3247b
ysmreg <33682244+ysmreg@users.noreply.github.com>
2024-07-06 00:37:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,16 +15,28 @@ listenPorts.forEach(port => {
       console.log('Connected to main server from port', port);
     });
 
+    mainServerSocket.on('error', (err) => {
+      console.error(`Main server socket error: ${err}`);
+    });
+
     socket.on('data', (data) => {
       const message = JSON.stringify({ minerIdentifier, data: data.toString('utf-8'), port });
       mainServerSocket.write(message);
     });
 
     mainServerSocket.on('data', (mainData) => {
-      const message = JSON.parse(mainData.toString('utf-8'));
-      if (minerSockets[message.minerIdentifier]) {
-        minerSockets[message.minerIdentifier].write(message.data);
+      try {
+        const message = JSON.parse(mainData.toString('utf-8'));
+        if (minerSockets[message.minerIdentifier]) {
+          minerSockets[message.minerIdentifier].write(message.data);
+        }
+      } catch (err) {
+        console.error(`Received data from main server is not valid JSON: ${mainData.toString('utf-8')}`);
       }
+    });
+
+    socket.on('error', (err) => {
+      console.error(`Miner socket error: ${err}`);
     });
 
     socket.on('close', () => {
@@ -40,5 +52,9 @@ listenPorts.forEach(port => {
 
   receiverServer.listen(port, '0.0.0.0', () => {
     console.log(`Receiver server is listening on port ${port}`);
+  });
+
+  receiverServer.on('error', (err) => {
+    console.error(`Receiver server error on port ${port}: ${err}`);
   });
 });
